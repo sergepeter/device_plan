3c48912e3ac0f2cb5fd1dcfa85745bcc3a1be5ca
Serge Peter <serge@serge.local>
2014-10-13 19:40:54 +0200

    add new files

---
+++ sql/device_plan_ddl.sql
@@ -0,0 +1,72 @@
+DROP TABLE IF EXISTS  area;
+DROP TABLE IF EXISTS  device;
+DROP TABLE IF EXISTS  floor_plan;
+
+CREATE TABLE IF NOT EXISTS `area` (
+  `area_id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `plan_id` int not null,
+  `name` int(100) NOT NULL,
+  `description` varchar(2000) NOT NULL
+) ;
+
+CREATE TABLE IF NOT EXISTS `device` (
+  `device_id`  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `code` varchar(200) NOT NULL,
+  `description` varchar(2000) NOT NULL,
+  `icon64x64_url` varchar(200) NOT NULL,
+  `image_url` varchar(200) NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `locationX` int(11) NOT NULL,
+  `locationY` int(11) NOT NULL,
+  `area_id` int(11) NOT NULL,
+  `plan_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+ 
+CREATE TABLE IF NOT EXISTS `floor_plan` (
+  `plan_id`  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `title` varchar(200) NOT NULL,
+  `description` varchar(2000) NOT NULL,
+  `width` int(11) NOT NULL,
+  `height` int(11) NOT NULL,
+  `coordN` double NULL,
+  `coordE` double NULL,
+  `plan_url` varchar(200) NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `area`
+--
+ALTER TABLE `area`
+ ADD PRIMARY KEY (`area_id`),  ADD UNIQUE KEY `name` (`name`) ;
+
+
+--
+-- Indexes for table `device`
+--
+ALTER TABLE `device`
+ ADD PRIMARY KEY (`device_id`), ADD UNIQUE KEY `code` (`code`), ADD KEY `area_id` (`area_id`), ADD KEY `plan_id` (`plan_id`);
+
+--
+-- Indexes for table `floor_plan`
+--
+ALTER TABLE `floor_plan`
+ ADD PRIMARY KEY (`floor_plan_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `device`
+--
+ALTER TABLE `device`
+ADD CONSTRAINT `area_relation` FOREIGN KEY (`area_id`) REFERENCES `area` (`area_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+ADD CONSTRAINT `floor_plan_relation` FOREIGN KEY (`plan_id`) REFERENCES `floor_plan` (`floor_plan_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ADD CONSTRAINT `area_relation_2` FOREIGN KEY (`plan_id`) REFERENCES `floor_plan` (`plan_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+
