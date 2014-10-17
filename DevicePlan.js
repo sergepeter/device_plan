7377f91876aee873428a9db22314533e5e3465fe
sergepeter <sergepeter@gmail.com>
2014-10-17 17:24:27 +0200

    Make it more dynamical

--- DevicePlan.js
+++ DevicePlan.js
@@ -1,65 +1,181 @@
+/**
+ * Main javascript code for device plan application
+ * @type @exp;evt@pro;srcElement@call;getAttribute
+ */
+
 var movingDeviceId;
 var deviceIsMoving;
 var deviceId = 1;
 var zoomAreaId;
 var zoomAreaIdActive = false;
+var currentPlanId;
 
+
+
+/**
+ * Main page initialisation function
+ * @returns {undefined}
+ */
 function init() {
+
     
+    
+    initPlan("plan1", 1835, 1260, 0, 0, "ressources/plan1.png");
+
+    printArea("area1", "m 264.26618,212.37336 306.73753,0 -0.47191,-25.48281 190.17727,0.94381 0.94381,283.61424 -403.94973,0.94381 0,-5.19095 -93.90888,0 z", "OK", "plan1");
+    printArea("area2", "m 767.31573,188.77817 67.01035,-0.47191 0.94381,24.53901 150.53734,0 1.41571,195.36821 -219.43531,-0.47191 z", "OK", "plan1");
+    printArea("area4", "m 1030.6381,208.59813 105.7065,0.47191 0.9438,192.53678 -108.5379,-0.94381 z", "OK", "plan1");
+    printArea("area5", "m 336.46747,776.77042 207.16581,-1.88762 0.94381,-302.9623 -212.35675,-2.35952 z", "OK", "plan1");
+    printArea("area6", "m 567.22848,472.39241 264.73808,0.4719 0.4719,22.17948 17.93235,0 -0.94381,256.24382 -16.51663,0 -0.47191,18.87615 -264.73808,0 -1.41571,-19.81996 -18.87616,0.94381 0.47191,-257.65953 18.87615,0.94381 z", "OK", "plan1");
+    printArea("area7", "m 264.73808,776.77042 494.55528,7.55046 c 0,0 3.30333,272.28852 1.41571,272.28852 -1.88761,0 -190.17727,0 -190.17727,0 l -0.4719,-23.1233 -305.79372,0.9438 z", "OK", "plan1");
+    printArea("area8", "m 767.31573,839.06173 c 2.83142,0 197.72773,0.94381 197.72773,0.94381 l -1.41571,193.00866 -127.41406,0.4719 -0.4719,25.4828 -67.48226,-0.9438 z", "OK", "plan1");
+    printArea("area9", "m 856.03366,411.5168 161.86304,1.41572 0.9438,419.99446 -161.86303,1.41571 z", "KO", "plan1");
+    printArea("area10", "m 970.7063,840.00554 c 2.35952,0 164.6945,0.9438 164.6945,0.9438 l 2.3595,90.13365 -70.7856,103.34691 -93.90888,-0.4719 z", "KO", "plan1");
+
     printDev("printer1", 400, 400, "printer", "OK", "plan1");
     printDev("printer2", 800, 600, "printer", "KO", "plan1");
     printDev("rack1", 1000, 800, "rack", "OK", "plan1");
     printDev("rack2", 800, 1000, "rack", "STANBY", "plan1");
-    
+
+}
+
+/**
+ * Init plan svg  objects
+ * @param {type} id
+ * @param {type} width
+ * @param {type} height
+ * @param {type} x
+ * @param {type} y
+ * @param {type} imgURL
+ * @returns {undefined}
+ */
+function initPlan(id, width, height, x, y, imgURL) {
+    console.log("Init Plan");
+
+    var style = "fill:grey;fill-opacity:0.1";
+
+    var img = document.getElementById("planImage");
+
+    img.setAttribute("x", x);
+    img.setAttribute("y", y);
+    img.setAttribute("id", "img" + id);
+    img.setAttribute("xlink:href", imgURL);
+    img.setAttribute("height", height);
+    img.setAttribute("width", width);
+
+    var planRect = document.getElementById("planRect");
+
+    planRect.setAttribute("x", x);
+    planRect.setAttribute("y", y);
+    planRect.setAttribute("id", id);
+    planRect.setAttribute("style", style);
+    planRect.setAttribute("height", height);
+    planRect.setAttribute("width", width);
+
+}
+
+/**
+ * Print a arean based on hidden example
+ * @param {type} id
+ * @param {type} d
+ * @param {type} status
+ * @param {type} parent
+ * @returns {undefined}
+ */
+function printArea(id, d, status, parent) {
+
+    console.log("Print area" + id);
+
+    var style = "fill:grey;fill-opacity:0.3;stroke:grey";
+    var area = document.getElementById("area");
+    var areaClone = area.cloneNode(true);
+
+    areaClone.setAttribute("id", id);
+    areaClone.setAttribute("d", d);
+
+    if (status == "OK") {
+        style = 'fill:green;fill-opacity:0.3;stroke:grey';
+    } else if (status == "KO") {
+        style = 'fill:red;fill-opacity:0.3;stroke:grey';
+    } else {
+        style = 'fill:grey;fill-opacity:0.3;stroke:grey';
+    }
+
+    areaClone.setAttribute("style", style);
+
+    var par = document.getElementById(parent);
+    par.parentNode.appendChild(areaClone);
+
+
 }
 
 
+/**
+ * Print a device on the screen
+ * @param {type} code
+ * @param {type} x
+ * @param {type} y
+ * @param {type} type
+ * @param {type} status
+ * @param {type} parent
+ * @returns {undefined}
+ */
 function  printDev(code, x, y, type, status, parent) {
-    
-    console.log("Insert Device" + code);
+
+    console.log("Print device" + code);
 
     var device = document.getElementById("device");
     var deviceClone = device.cloneNode(true);
 
     deviceClone.setAttribute("style", "fill:blue;fill-opacity:1");
     deviceClone.setAttribute("x", x - 16);
-    deviceClone.setAttribute("y", y- 16);
+    deviceClone.setAttribute("y", y - 16);
     deviceClone.setAttribute("id", code)
-    
+
     if (status == "OK") {
-        deviceClone.setAttribute("xlink:href","ressources/" + type + "OK.png");
+        deviceClone.setAttribute("xlink:href", "ressources/" + type + "OK.png");
     } else if (status == "KO") {
-        
-         deviceClone.setAttribute("xlink:href","ressources/" + type + "KO.png");
-        
+
+        deviceClone.setAttribute("xlink:href", "ressources/" + type + "KO.png");
+
     } else {
-         deviceClone.setAttribute("xlink:href","ressources/" + type + ".png");
+        deviceClone.setAttribute("xlink:href", "ressources/" + type + ".png");
     }
-    
+
     var par = document.getElementById(parent);
     par.parentNode.appendChild(deviceClone);
-    
+
 }
 
-function addDevice(evt, parent) {
 
+/**
+ * Add device and print it
+ * @param {type} evt
+ * @param {type} parent
+ * @returns {undefined}
+ */
+function addDevice(evt) {
     console.log("Open add dialog box");
-     printDev("device_" + deviceId++,  (evt.clientX),  (evt.clientY), "printer", "STANDBY", "plan1");
-    
+    printDev("device_" + deviceId++, (evt.clientX), (evt.clientY), "printer", "STANDBY", "plan1");
 }
 
+/**
+ * Mouse funny stuff
+ * @param {type} evt
+ * @returns {undefined}
+ */
 function startMoveDevice(evt) {
-    if(event.button == 2) {
-        modifyDevice(evt);   
+    if (event.button == 2) {
+        modifyDevice(evt);
     } else {
-    //deviceIsMoving = true;
-    movingDeviceId = evt.srcElement.getAttribute("id");
-    console.log("Start moving " + movingDeviceId);
+        //deviceIsMoving = true;
+        movingDeviceId = evt.srcElement.getAttribute("id");
+        console.log("Start moving " + movingDeviceId);
     }
 }
 
 function stopMoveDevice(evt) {
-    
+
     if (deviceIsMoving) {
         console.log("Stop moving " + movingDeviceId);
         deviceIsMoving = false;
@@ -67,51 +183,53 @@ function stopMoveDevice(evt) {
     }
 }
 
-
-
 function modifyDevice(evt) {
-   if (!deviceIsMoving) {
-       console.log("Modify device");
-       
-   }
-
-
+    if (!deviceIsMoving) {
+        console.log("Modify device");
+    }
 }
-    
+
 function moveDevice(evt) {
-   
-    if ( movingDeviceId != null){
-         console.log("Moving  ..." + movingDeviceId);
+    if (movingDeviceId != null) {
+        console.log("Moving  ..." + movingDeviceId);
         var mov = document.getElementById(movingDeviceId);
         mov.setAttribute("x", evt.clientX);
         mov.setAttribute("y", evt.clientY);
-        deviceIsMoving=true;
-   
+        deviceIsMoving = true;
     }
-    
 }
+var oriFill;
 
 function  overArea(evt) {
     zoomAreaIdActive = true;
     zoomAreaId = evt.srcElement.getAttribute("id");
+
     //width="1835" height="1260"
     //evt.srcElement.setAttribute("transform","scale(1.005)" );
-    evt.srcElement.setAttribute("style","fill:grey;fill-opacity:0.8;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" );
-    
-   // console.log("Start highlight " + zoomAreaId);
+    oriFill = evt.srcElement.getAttribute("style");
+
+    evt.srcElement.setAttribute("style", "fill:grey;fill-opacity:0.3;stroke:grey");
+
+    // console.log("Start highlight " + zoomAreaId);
 }
 
 function  outArea(evt) {
     if (zoomAreaId != null) {
-      //  console.log("Stop moving " + zoomAreaId);
+        //  console.log("Stop moving " + zoomAreaId);
         //evt.srcElement.setAttribute("transform","scale(1)" );
-          evt.srcElement.setAttribute("style","fill:grey;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1" );
-   
+        evt.srcElement.setAttribute("style", oriFill);
+
         zoomAreaIdActive = false;
         zoomAreaId = null;
     }
 }
 
-
+function resetAllDragOperation() {
+   if (deviceIsMoving) {
+        console.log("Stop moving " + movingDeviceId);
+        deviceIsMoving = false;
+        movingDeviceId = null;
+    }
+}
 
 
--- index.php
+++ index.php
@@ -4,7 +4,6 @@ $db = new PDO('mysql:dbname=device_map;host=localhost', 'root', 'mysql');
 createDefaultData($db);
 ?>
 
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -12,147 +11,110 @@ createDefaultData($db);
         <title>Device Map</title>
         <script src="DevicePlan.js"></script>
         <script src="js/jquery-2.1.1.js"></script>
-        <link rel="stylesheet" href="js/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="js/bootstrap/css/bootstrap-theme.min.css">
-        <script src="js/bootstrap/js/bootstrap.min.js"></script>
-        <script src="js/bootstrap/js/bootbox.min.js"></script>
-      
+
+        <link href="js/bootstrap/css/bootstrap.css" rel="stylesheet">
+        <style type="text/css">
+            body {
+                padding-top: 0px;
+                padding-bottom: 0px;
+            }
+            .sidebar-nav {
+                padding: 9px 0;
+            }
+
+            @media (max-width: 980px) {
+                /* Enable use of floated navbar text */
+                .navbar-text.pull-right {
+                    float: none;
+                    padding-left: 5px;
+                    padding-right: 5px;
+                }
+            }
+        </style>
+
+
+
     </head>
 
     <body>
+        <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="navbar-inner">
+                <div class="container-fluid">
+                    <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="brand" href="#">Device Plan</a>
+                    <div class="nav-collapse collapse">
+                        <p class="navbar-text pull-right">
+                            Source hosted on <a href="https://github.com/sergepeter/device_plan" target="_blank">Github</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
 
+        <svg  width="1800"
+              height="1260"
+              id="svgplan"
+              version="1.1">
+        <defs id="defPlan" />
 
-        <svg
-            width="1800"
-            height="1260"
-            id="svgplan"
-            version="1.1"
-            inkscape:version="0.48.5 r10040"
-            sodipodi:docname="plan1.svg">
-        <defs
-            id="defs4" />
+        <g id="planGroup">
 
-        <g
-            id="layer1"
-            >
         <image
             y="0"
             x="0"
-            id="planimg1"
-            xlink:href="ressources/plan1.png"
-            height="1260"
-            width="1835" 
-            />
+            id="planImage"
+            xlink:href=""
+            height=""
+            width=""/>
 
-        <rect id="plan1" width="1835" height="1260" style="fill:blue;fill-opacity:0.1"  y="0"
-              x="0" onclick="addDevice(evt, 'plan1')"  onmousemove="moveDevice(evt)" />
+        <rect id="planRect" 
+              width="" 
+              height="" 
+              style="fill:grey;fill-opacity:0.1"  
+              y="0"
+              x="0" 
+              onclick="addDevice(evt)"  
+              onmousemove="moveDevice(evt)" />
 
 
-        <path 
-            style="fill:grey;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-            d="m 258.89232,206.1828 310.21459,3.42148 0,-22.80989 190.46263,0 2.28099,225.81797 -408.29714,55.88424 -94.66107,-1.14049 z"
-            id="area1" 
-            onmouseover="overArea(evt)" 
-            onmouseout="outArea(evt)" 
-            onmousemove="moveDevice(evt)"
-            onclick="addDevice(evt, 'plan1')" 
-            />
-        <path
-            style="fill:grey;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-            d="m 765.27202,189.07537 69.57018,-1.14049 0,23.95039 184.7602,-1.14049 -1.1405,197.3056 -249.7684,1.14049 z"
-            onmouseover="overArea(evt)" 
-            onmouseout="outArea(evt)" 
-            onmousemove="moveDevice(evt)"
-            onclick="addDevice(evt, 'plan1')" 
-            id="area2"
-            />
-        <path
-            style="fill:grey;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-            d="m 1029.8668,211.88527 106.066,0 1.1405,190.46263 -106.066,-1.14049 z"
-            onmouseover="overArea(evt)" 
-            onmouseout="outArea(evt)" 
-            onmousemove="moveDevice(evt)"
-            onclick="addDevice(evt, 'plan1')" 
-            id="area9"
-            />
-        <path
-            style="fill:grey;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-            d="m 544.01602,471.91809 309.0741,3.42148 -1.1405,296.52865 -304.51211,0 z"
-            onmouseover="overArea(evt)" 
-            onmouseout="outArea(evt)" 
-            onmousemove="moveDevice(evt)"
-            onclick="addDevice(evt, 'plan1')" 
-            id="area3"
+        <path id="area" 
+              style="fill:grey;fill-opacity:0.3;stroke:grey"
+              d="m 1,1"
+              ondblclick="addDevice(evt)"
+              onmouseover="overArea(evt)" 
+              onmouseout="outArea(evt)" 
+              onmousemove="moveDevice(evt)"
+              </g>
 
-            />
-        <path
-            style="fill:grey;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-            d="m 263.4543,777.5707 587.35483,-2.28099 -4.56198,60.44622 -80.97513,0 0,223.53697 -193.88412,-2.281 -2.28099,-21.6694 -305.65261,-1.1405 z"
-            onmouseover="overArea(evt)" 
-            onmouseout="outArea(evt)" 
-            onmousemove="moveDevice(evt)"
-            onclick="addDevice(evt, 'plan1')" 
-            id="area4"
-
-            />
-        <path
-            style="fill:grey;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-            d="m 767.55301,839.15742 198.44609,0 0,196.16508 -128.87591,1.1405 -1.1405,25.0909 -67.28919,-2.281 z"
-            onmouseover="overArea(evt)" 
-            onmouseout="outArea(evt)" 
-            onmousemove="moveDevice(evt)"
-            onclick="addDevice(evt, 'plan1')" 
-            id="area5"
-
-            />
-        <path
-            style="fill:grey;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-            d="m 968.28009,836.87643 166.51221,0 2.281,98.08255 -71.8511,102.64452 -96.94211,-3.4215 z"
-            onmouseover="overArea(evt)" 
-            onmouseout="outArea(evt)" 
-            onmousemove="moveDevice(evt)"
-            onclick="addDevice(evt, 'plan1')" 
-            id="area6"
-            />
-        <path
-            style="fill:grey;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-            d="m 330.74349,473.05858 210.99154,-31.93385 3.42149,334.16498 -212.13204,-1.1405 3.42149,-264.59479 z"
-            onmouseover="overArea(evt)" 
-            onmouseout="outArea(evt)" 
-            onmousemove="moveDevice(evt)"
-            onclick="addDevice(evt, 'plan1')" 
-            id="area7" 
-            />
-        <path
-            style="fill:grey;fill-opacity:0.5;stroke:#000000;stroke-width:1px;stroke-linecap:butt;stroke-linejoin:miter;stroke-opacity:1"
-            d="m 846.24715,408.05038 173.35525,1.14049 1.1405,86.67761 35.3553,-1.1405 1.1405,260.03282 -36.4958,0 -4.562,77.55365 -165.37177,5.70247 z"
-            onmouseover="overArea(evt)" 
-            onmouseout="outArea(evt)" 
-            onmousemove="moveDevice(evt)"
-            onclick="addDevice(evt, 'plan1')" 
-            id="area8"
-            />
-            <!--rect id="device" width="32" height="32" oncontextmenu="return false;" onmousedown="startMoveDevice(evt)"  onmouseup="stopMoveDevice(evt)" style="fill:blue;fill-opacity:0" x="100" y="100"/-->
-            
-        </g>
-        
         <image id="device" 
-            width="32" 
-            height="32" 
-            oncontextmenu="return false;" 
-            onmousedown="startMoveDevice(evt)"  
-            onmouseup="stopMoveDevice(evt)" 
-            x="-100" 
-            y="-100" xlink:href="ressources/printerOK.png">     
+               width="32" 
+               height="32" 
+               oncontextmenu="return false;" 
+               onmousedown="startMoveDevice(evt)"  
+               onmouseup="stopMoveDevice(evt)" 
+               x="-100" 
+               y="-100" xlink:href="ressources/printerOK.png">     
         </image>
 
-            
-               
+        </g>
+        </svg>
+
         <script>
             $(document).ready(function () {
-               init();
+                init();
+            });
+            $(document).keyup(function (e) {
+
+                if (e.keyCode == 27) {
+                    resetAllDragOperation();
+                }   // esc
             });
         </script>
+
     </body>
 
 </html>
